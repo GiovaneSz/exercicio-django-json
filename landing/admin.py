e295c6db094cbb230447c6b1e0e858decce9af83
giovane <giovanesantlima@gmail.com>
2019-09-03 17:47:15 -0300

    criar projeto

---
+++ landing/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from aluno.models import Aluno 
+# Register your models here.
---
+++ landing/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Aluno(models.model)
+    nome= models.Charfield(
+        max_length=255,
+
+    )
+    idade = models.Integerfield()
+        (
+        max_length=255,
+    )
+ 
